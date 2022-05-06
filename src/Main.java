038619c37d9ed2e1899528fe34ebc9c0ff879efb
GTeasera <87891550+GTeasera@users.noreply.github.com>
2022-05-07 01:07:19 +0300

    07/05/2022
    Threading. Volatile. Lesson 2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
@@ -11,9 +13,13 @@ public class Main {
         for(int i = 0; i<10; i++) {
             System.out.println("Tread #1 - " + i);
         }
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+
+        myThread.shutdown();
     }
 }
-
 
 // 2 Way with using interface Runnable
 class Runner implements Runnable{
@@ -32,14 +38,22 @@ class Runner implements Runnable{
 
 // 1 Way with class which extends Thread
 class MyThread extends Thread{
-    public void run(){
-        for(int i = 0; i<10; i++) {
+    private volatile boolean running = true;
+    int i = 0;
+
+    public void run() {
+        while (running) {
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
             System.out.println("Tread #2 - " + i);
+            i++;
         }
+    }
+
+    public void shutdown(){
+        this.running = false;
     }
 }
